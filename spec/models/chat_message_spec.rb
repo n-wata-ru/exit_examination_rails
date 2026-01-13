591f80c43b3293532e5d5836f1c2753bb226869d
fi-naoki-watanabe <naoki.watanabe@findy.co.jp>
2026-01-13 18:26:24 +0900

    chatMessagesモデルのrspecにテストを追加 合わせてmodel側に不足していたvalidateやhas_manyの記述を追加

--- spec/models/chat_message_spec.rb
+++ spec/models/chat_message_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ChatMessage, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:chat_thread) { FactoryBot.create(:chat_thread, user: user) }
+  let(:chat_message) { FactoryBot.create(:chat_message, user: user, chat_thread: chat_thread) }
+
+  subject { FactoryBot.build(:chat_message, user: user, chat_thread: chat_thread) }
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:chat_thread) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of(:user) }
+    it { should validate_presence_of(:chat_thread) }
+    it { should validate_presence_of(:role) }
+    it { should validate_presence_of(:content) }
+
+    describe 'dependent destroy' do
+      it 'chat_threadが削除されたら関連するchat_messagesも削除されること' do
+        chat_thread.chat_messages.create!(
+          user: user,
+          content: 'Hello',
+          role: 'user'
+        )
+
+        expect { chat_thread.destroy }.to change { ChatMessage.count }.by(-1)
+      end
+    end
+  end
 end
