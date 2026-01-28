a1f7b85b3b5f6157aa7ac0368897015e0ce75d0d
fi-naoki-watanabe <naoki.watanabe@findy.co.jp>
2026-01-28 18:15:50 +0900

    不要なテーブルが残っていたので削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_28_074225) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_28_074312) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_28_074225) do
     t.bigint "user_id", null: false
     t.index ["chat_thread_id"], name: "index_chat_messages_on_chat_thread_id"
     t.index ["user_id"], name: "index_chat_messages_on_user_id"
+  end
+
+  create_table "chat_rate_limits", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "requested_at"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chat_rate_limits_on_user_id"
   end
 
   create_table "chat_threads", force: :cascade do |t|
@@ -137,6 +145,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_28_074225) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chat_messages", "chat_threads"
   add_foreign_key "chat_messages", "users"
+  add_foreign_key "chat_rate_limits", "users"
   add_foreign_key "chat_threads", "users"
   add_foreign_key "coffee_beans", "origins"
   add_foreign_key "coffee_beans", "users"
--- spec/factories/chat_rate_limits.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :chat_rate_limit do
-    user { nil }
-    requested_at { "2026-01-28 16:43:12" }
-  end
-end
